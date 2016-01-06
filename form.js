fa574d02f79a592784cfa820ecb4fba17504766d
duke gallo <dukegallo@gmail.com>
2016-01-06 12:44:45 -0800

    Cleaned up untracked files.

--- form.js
+++ form.js
@@ -1,13 +1,15 @@
-document.getElementById('search-button').addEventListener('click', function() {
+document.getElementById('search-button').addEventListener('click', function(e) {
+  e.preventDefault();
   var xhr = new XMLHttpRequest();
   xhr.onload = function() {
     if(xhr.status === 200) {
-      result = document.getElementById('result');
-      result.textContent = xhr.responseText;
+      // result = document.getElementById('result');
+      // result.textContent = xhr.responseText;
+      console.log(xhr.responseText);
     }
   }
   var keyword = document.getElementById('keyword').value;
   console.log(keyword);
-  xhr.open('POST', 'http://127.0.0.1:1337/query', true);
+  xhr.open('POST', 'http://localhost:1337/query', true);
   xhr.send(keyword);
 }, false);
