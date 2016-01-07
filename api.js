cdce904a5388fa00960d4977d7f67e0bf923aa21
duke gallo <dukegallo@gmail.com>
2016-01-07 10:23:19 -0800

    Index with search and recipe functionality.

--- api.js
+++ api.js
@@ -1,40 +1,34 @@
 var express = require('express');                    //setting up router
 var router = express.Router();
 var request = require('request');
+var rp = require('request-promise');
 var bodyParser = require('body-parser');
 var textParser = bodyParser.text();
- 
-// function callback(error, response, body) {
-//   if (!error && response.statusCode == 200) {
-//     var info = JSON.parse(body);
-//   }
-// }
- 
-//request(options, callback);
 
-router.post('/', textParser, function(req,res) {    //bringing in API
-  var input = req.body;
-  var url = 'http://api.bigoven.com/recipes/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v&pg=1&rpp=10&any_kw=' + input;
-  console.log(url);
-  var options = {
-      url: url,
-      headers: {
-        'Accept': 'application/json',                //accepting Json not XHTML      
-        'Content-Type': 'application/json'
+    router.post('/', textParser, function(req,res) {    //bringing in API
+      var input = req.body;
+      var url = 'http://api.bigoven.com/recipes/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v&pg=1&rpp=10&any_kw=' + input;
+      console.log(url);
+      var options = {
+        url: url,
+        headers: {
+          'Accept': 'application/json',                //accepting Json not XHTML      
+          'Content-Type': 'application/json'
       }
-    };
+  };
+
   request.get(options, function (error, response, body) {
-    if (!error && res.statusCode == 200) {
+      if (!error && res.statusCode == 200) {
         var data = JSON.parse(body);
         var ids = [];
         for (var i = 0; i < 6; i++) {
-            ids.push(data.Results[i].RecipeID);
-        }
-        
-        var recipeUrl = 'http://api.bigoven.com/recipe/' + ids[0] + '/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v';
-        var recipeRequest = {
-            url: recipeUrl,
-            headers: {
+          ids.push(data.Results[i].RecipeID);
+      }
+
+      var recipeUrl = 'http://api.bigoven.com/recipe/' + ids[0] + '/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v';
+      var recipeRequest = {
+          url: recipeUrl,
+          headers: {
                 'Accept': 'application/json',                //accepting Json not XHTML      
                 'Content-Type': 'application/json'
             }
@@ -43,16 +37,23 @@ router.post('/', textParser, function(req,res) {    //bringing in API
             console.log(body);
         });
 
-        
-        
-        
-        res.send(body);
 
-       
-       console.log('call has been made');
-       
-    }
-  });
+
+    Promise.all([ recipeUrl ]).then(function(body) {
+     console.log(body);
+ });
+
+
+
+
+
+    res.send(body);
+
+
+    console.log('call has been made');
+
+}
+});
 });
 
 module.exports = router;
