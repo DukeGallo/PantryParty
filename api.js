5ade001705e9e0b04b1054a59d937ca70789547a
duke gallo <dukegallo@gmail.com>
2016-01-12 16:23:24 -0800

    dynamic search results with live clear.

--- api.js
+++ api.js
@@ -5,43 +5,43 @@ var rp = require('request-promise');
 var bodyParser = require('body-parser');
 var textParser = bodyParser.text();
 
-    router.post('/', textParser, function(req,res) {
-      var input = req.body;
-      var url = 'http://api.bigoven.com/recipes/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v&pg=1&rpp=10&any_kw=' + input;
-      console.log(url);
-      var options = {
-        url: url,
-        headers: {
-          'Accept': 'application/json',                     
-          'Content-Type': 'application/json'
-      }
-    };
+router.post('/', textParser, function(req,res) {
+  var input = req.body;
+  var url = 'http://api.bigoven.com/recipes/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v&pg=1&rpp=10&any_kw=' + input;
+  console.log(url);
+  var options = {
+    url: url,
+    headers: {
+      'Accept': 'application/json',                     
+      'Content-Type': 'application/json'
+    }
+  };
 
-    request.get(options, function (error, response, body) {
-      if (!error && res.statusCode == 200) {
-        var data = JSON.parse(body);
-        var ids = [];
-        for (var i = 0; i < 6; i++) {
-          ids.push(data.Results[i].RecipeID);
-      }
-
-      var recipeUrl = 'http://api.bigoven.com/recipe/' + ids[0] + '/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v';
-      var recipeRequest = {
-          url: recipeUrl,
-          headers: {
-                'Accept': 'application/json',                     
-                'Content-Type': 'application/json'
-            }
-        };
-        request.get(recipeRequest, function(error, response, body) {
-            console.log(body);
-        });
-
-    res.send(body);
-    console.log('call has been made');
-
-}
+  request.get(options, function (error, response, body) {
+    if (!error && res.statusCode == 200) {
+      res.send(body);
+      console.log('call has been made');
+    }
+  });
 });
+
+router.get('/:recipeID', function(req, res) {
+  var recipeID = req.params.recipeID;
+  var url = 'http://api.bigoven.com/recipe/' + recipeID + '/?api_key=5CWa265SNXT70ACQbGJ2CFrpi1c6j85v';
+  var options = {
+    url: url,
+    headers: {
+      'Accept': 'application/json',                     
+      'Content-Type': 'application/json'
+    }
+  };
+
+  request.get(options, function (error, response, body) {
+    if (!error && res.statusCode == 200) {
+      res.send(body);
+
+    }
+  });
 });
 
 module.exports = router;
--- form.js
+++ form.js
@@ -6,26 +6,65 @@ document.getElementById('search-button').addEventListener('click', function(e) {
   xhr.onload = function() {
     if(xhr.status === 200) {
       console.log(xhr.responseText);
-    }
+      var recipes = JSON.parse(xhr.responseText);
+      var holder = document.getElementById('recipe-holder');
+      holder.innerHTML = '';
+      for (var i = 0; i < recipes.Results.length; i++) {
+        
+        var col = document.createElement('div');
+        col.setAttribute('class', 'col s12 m4');
+        // col.textContent = recipes.Results[i].RecipeID + ' ' + recipes.Results[i].Title + ' ' + recipes.Results[i].ImageURL;
+        
+        var iconBlock = document.createElement('div');
+        iconBlock.setAttribute('class', 'icon-block');
+        
+        var light = document.createElement('p');
+        light.setAttribute('class', 'light');
+        
+        var card = document.createElement('div');
+        card.setAttribute('class', 'card');
+      
+        var cardImage = document.createElement('div');
+        cardImage.setAttribute('class', 'card-image waves-effect waves-block waves-light');
+       
+        var activator = document.createElement('img');
+        activator.setAttribute('class', 'activator');
+        activator.setAttribute('src', recipes.Results[i].ImageURL);
+
+        var cardContent = document.createElement('div');
+        cardContent.setAttribute('class', 'card-content');
+       
+        var cardTitle = document.createElement('span');
+        cardTitle.setAttribute('class', 'card-title activator grey-text text-darken-4');
+        cardTitle.textContent = recipes.Results[i].Title;
+       
+        var materialIcons = document.createElement('i');
+        materialIcons.setAttribute('class', 'material-icons right');
+        materialIcons.textContent = 'more_vert';
+        // materialIcons.addEventListener('click', function(e);
+        // materialIcons.textContent = recipes.Results[i].Title;
+
+        cardTitle.appendChild(materialIcons);
+        cardContent.appendChild(cardTitle);
+        cardImage.appendChild(activator);
+        card.appendChild(cardImage);
+        card.appendChild(cardContent);
+        light.appendChild(card);
+        iconBlock.appendChild(light);
+        col.appendChild(iconBlock);
+        holder.appendChild(col);
+      }
+      document.getElementById('card').scrollIntoView();
+    } 
   }
 
+
+
+
+      
   var keyword = document.getElementById('keyword').value;
   console.log(keyword);
 
   xhr.open('POST', 'http://localhost:1337/query', true);
   xhr.send(keyword);
 }, false);
-
-var recipe = document.getElementByTagName('span')[0,1,2,3,4,5];
-
-document.getElementById('recipe-1-outside').addEventListener('load', function(e) {
-  e.preventDefault();
-
-  var xhr = new XMLHttpRequest();
-
-  xhr.onload = function() {
-    if(xhr.status === 200) {
-      console.log(xhr.body);
-    }
-  }
-
